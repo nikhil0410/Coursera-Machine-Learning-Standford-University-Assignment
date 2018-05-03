5310a60ae843148ae0790ad59112627e9bb00fce
Nikhil <ernikhilgupta2015@gmail.com>
2018-05-03 18:37:55 +0530

    week 2 assignment

---
+++ Coursera-Machine-Learning-Week-2-assignment/ex1/warmUpExercise.m
@@ -0,0 +1,23 @@
+function A = warmUpExercise()
+%WARMUPEXERCISE Example function in octave
+%   A = WARMUPEXERCISE() is an example function that returns the 5x5 identity matrix
+
+A = [];
+% ============= YOUR CODE HERE ==============
+% Instructions: Return the 5x5 identity matrix 
+%               In octave, we return values by defining which variables
+%               represent the return values (at the top of the file)
+%               and then set them accordingly. 
+
+%A = [1 2 3 4 5; 1 2 2 2 1; 1 2 3 2 1; 1 2 3 4 2; 9 8 7 6 5]
+A = eye(5);
+A
+
+
+
+
+
+% ===========================================
+
+
+end
